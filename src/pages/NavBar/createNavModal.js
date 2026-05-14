040fea2fcca1d0e16671712c0a2ceb56d5f8d37b
PFS <peintnerfamilysites@gmail.com>
2026-05-14 17:46:31 +0000

    Fixed Modal Open and close for Mobile

--- src/pages/NavBar/createNavModal.js
+++ src/pages/NavBar/createNavModal.js
@@ -1,16 +1,17 @@
 // Create the mobile navigation menu modal and return the html element to be rendered on the page.
-export default function createNavModal() {
+export default function createNavModal(id) {
   // Create a container for the mobile navigation menu and add the appropriate class to it.
   const navModalContainer = document.createElement("div");
   navModalContainer.classList.add("nav-modal-container");
-  navModalContainer.id = "mobile-nav-menu";
+  navModalContainer.id = id;
   // Creat an img element for the close button and add the appropriate class to it.
   const closeButton = document.createElement("img");
   closeButton.classList.add("nav-modal-close-button");
   closeButton.src = "https://img.icons8.com/ios-glyphs/30/000000/macos-close.png";
   closeButton.alt = "Close Button";
   closeButton.addEventListener("click", () => {
-    // When the close button is clicked, remove the class "open" from the modal container to hide it.
+    // When the close button is clicked close modal
+    navModalContainer.remove();
   });
   // Append the close button to the modal container.
   navModalContainer.appendChild(closeButton);
--- src/utils/modalOpen.js
+++ src/utils/modalOpen.js
@@ -3,10 +3,17 @@ import createNavModal from "../pages/NavBar/createNavModal.js";
 // This function will open a modal 
 export default function modalOpen(modalId) {
   if (modalId === "mobile-nav-menu") {
-    // Create the modal element
-    const modal = createNavModal();
-    // Append the modal to the modal container
     const modalContainer = document.getElementById("modal-container");
+    // Get current modal if it exists
+    const currentModal = document.getElementById(modalId);
+    if (currentModal) {
+      // If the modal already exists, clear its content to close it.
+      currentModal.remove();
+      return;
+    }
+    // Create the modal element
+    const modal = createNavModal(modalId);
+    // Append the modal to the modal container
     modalContainer.appendChild(modal);
   }
 }
