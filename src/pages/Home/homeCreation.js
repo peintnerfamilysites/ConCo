4fe0f0eb5965d13939b4f99dc9baf0fe8cbe63f7
PFS <peintnerfamilysites@gmail.com>
2026-05-21 00:04:30 +0000

    Homepage work

--- src/pages/Home/homeCreation.js
+++ src/pages/Home/homeCreation.js
@@ -1,6 +1,5 @@
 // Import the functions to create the header, body, and footer for the home page.
 import createHomeBody from "./homeBody.js";
-import createHomeFooter from "./homeFooter.js";
 import createNavBar from "../NavBar/createNavBar.js";
 
 // This will create the home page and return the html element to be rendered on the page.
@@ -14,14 +13,11 @@ export default function homeCreation(screenSize) {
   const navBar = createNavBar(screenSize);
   // Call the home body creation function to create the body for the home page.
   const homeBody = createHomeBody();
-  // Call the home footer creation function to create the footer for the home page.
-  const homeFooter = createHomeFooter();
   // Create a container for the home page and append the header, body, and footer to it.
   const homeContainer = document.createElement("div");
   homeContainer.classList.add("home-container");
   homeContainer.appendChild(navBar);
   homeContainer.appendChild(homeBody);
-  homeContainer.appendChild(homeFooter);
   // Return the home container to be rendered on the page.
   return homeContainer;
 }
